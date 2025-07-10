620e22e09b9cb3f9e0382f1b700831842b511e00
julas23 <julianoas@.com>
2025-07-10 09:03:27 +0100

    adding python modules

--- etc/nixos/modules/packages.nix
+++ etc/nixos/modules/packages.nix
@@ -22,16 +22,20 @@ pkgs: with pkgs; [
 
 #### system
   alsa-firmware alsa-utils arandr networkmanagerapplet cronie htop inxi exo eza font-manager freerdp git
-  glxinfo lsof mc micro pstree pavucontrol dmidecode lshw neofetch lm_sensors
+  glxinfo lsof mc micro pstree pavucontrol dmidecode lshw neofetch lm_sensors xfce.thunar
   vulkan-loader vulkan-tools xorg.xrandr microcode-amd
 
 #### tools
   wavebox alacritty ansible tmux termite zsh fish wlogout swaybg kdePackages.okular grimblast btop
   popsicle blueman kdePackages.dolphin nano nettools nfs-utils mlocate p7zip curl nvme-cli usbutils
   rpi-imager rdesktop remmina system-config-printer unrar unzip virtualbox numlockx file dnsutils whois
-  pkgs.gnome-calculator wget xdg-utils xdg-desktop-portal
+  pkgs.gnome-calculator wget xdg-utils xdg-desktop-portal psmisc procps sqlite
+
+#### python modules
+  python3 python3Packages.requests python3Packages.termcolor
 
 #### opcional
+  polychromatic
   lsyncd
   cava
   conky
--- etc/nixos/modules/ryzen.nix
+++ etc/nixos/modules/ryzen.nix
@@ -4,8 +4,10 @@
   services.xserver.displayManager.gdm.enable = true;
   services.xserver.desktopManager.gnome.enable = true;
   services.xserver.videoDrivers = [ "nvidia" ];
+
   networking.hostName = "ryzen";
 
+  hardware.openrazer.enable = true;
   hardware.graphics.enable = true;
   hardware.nvidia = {
     modesetting.enable = true;
@@ -15,8 +17,12 @@
     package = config.boot.kernelPackages.nvidiaPackages.stable;
   };
   boot.initrd.kernelModules = [ "nvidia_modeset" "nvidia" "nvidia_uvm" "nvidia_drm" ];
-
- environment.systemPackages = with pkgs; [ gnome-tweaks gnome-console nautilus desktop-file-utils xdotool wmctrl whisper-cpp python3 python3Packages.pip python3Packages.torchWithCuda python3Packages.transformers python3Packages.accelerate python3Packages.datasets ];
+  boot.kernelModules = ["i2c-dev" "ddcci_backlight"];
+  services.udev.extraRules = ''
+    SUBSYSTEM=="hidraw", ATTRS{idVendor}=="04e8", ATTRS{idProduct}=="008b", MODE="0666"
+    SUBSYSTEM=="hidraw", ATTRS{idVendor}=="04e8", ATTRS{idProduct}=="007f", MODE="0666"
+  '';
+ environment.systemPackages = with pkgs; [ gnome-tweaks gnome-console nautilus desktop-file-utils xdotool wmctrl whisper-cpp python3 python3Packages.pip python3Packages.torchWithCuda python3Packages.transformers python3Packages.accelerate python3Packages.datasets openrazer-daemon hidapi libusb1 pipx ];
 
   environment.variables = {
     WLR_NO_HARDWARE_CURSORS = "1";
