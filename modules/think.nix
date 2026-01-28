a872e5e7bbfa913248a7a259b32eedded3c63962
Juliano Alves dos Santos <92172251+julas23@users.noreply.github.com>
2026-01-28 14:40:55 -0300

    Delete modules directory

--- modules/think.nix
+++
@@ -1,6 +0,0 @@
-{ config, lib, pkgs, ... }:
-
-lib.mkIf (config.install.system.host == "think") {
-
-  networking.hostName = "think";
-}
--- modules/vai.sh
+++
@@ -1,5 +0,0 @@
-#!/run/current-system/sw/bin/bash
-clear
-nix-collect-garbage -d
-nix-store --optimize
-nixos-rebuild switch --flake .#hp --refresh --option extra-substituters https://cosmic.cachix.org/ --option extra-trusted-public-keys cosmic.cachix.org-1:D7qyUR+MS4dnq2SKL/M/1m798E8P5qfbxP57FPpLGXg=
