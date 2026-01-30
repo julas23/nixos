7d46a6023049f0c8f16c5778b2440bbdb8d13985
Juliano Alves dos Santos <92172251+julas23@users.noreply.github.com>
2026-01-30 16:54:44 +0000

    Enable nix-ld with necessary libraries
    Enable nix-ld and add required libraries for it.

--- modules/base.nix
+++ modules/base.nix
@@ -153,6 +153,51 @@
 
   programs.adb.enable = true;
 
+  programs.nix-ld.enable = true;
+  programs.nix-ld.libraries = with pkgs; [
+    stdenv.cc.cc
+    zlib
+    fuse
+    alsa-lib
+    at-spi2-atk
+    cairo
+    cups
+    dbus
+    expat
+    fontconfig
+    freetype
+    gdk-pixbuf
+    glib
+    gtk3
+    libGL
+    libappindicator-gtk3
+    libdrm
+    libnotify
+    libpulseaudio
+    libuuid
+    libusb1
+    libxkbcommon
+    mesa
+    nspr
+    nss
+    pango
+    pipewire
+    systemd
+    xorg.libX11
+    xorg.libXScrnSaver
+    xorg.libXcomposite
+    xorg.libXcursor
+    xorg.libXdamage
+    xorg.libXext
+    xorg.libXfixes
+    xorg.libXi
+    xorg.libXrandr
+    xorg.libXrender
+    xorg.libXtst
+    xorg.libxcb
+    xorg.libxshmfence
+  ];
+
   systemd.user.services.pixel-bridge = {
     description = "Reverse Tethering for Pixel 8 Pro";
     serviceConfig = {
