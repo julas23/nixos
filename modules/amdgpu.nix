6b72d9a9d9979adaf6d93be65f9bd4f6526ad1c3
Juliano Alves dos Santos <92172251+julas23@users.noreply.github.com>
2026-01-31 13:02:44 +0000

    Update amdgpu configuration with new settings
    Added kernel parameter for IOMMU and enabled redistributable firmware.

--- modules/amdgpu.nix
+++ modules/amdgpu.nix
@@ -2,41 +2,28 @@
 
 lib.mkIf (config.install.system.video == "amdgpu") {
 
+  boot.kernelParams = [ "iommu=pt" ];
   boot.initrd.kernelModules = [ "amdgpu" ];
-
   services.xserver.videoDrivers = [ "amdgpu" ];
-
   hardware.graphics.enable = true;
   hardware.amdgpu.opencl.enable = true;
+  hardware.enableRedistributableFirmware = true;
 
-  hardware.opengl = {
-    enable = true;
-    #driSupport = true;
-    #driSupport32Bit = true;
-    #extraPackages = with pkgs; [
-      #rocmPackages.clr.icd
-      #amdvlk
-      #rocmPackages.llvm
-    #];
-    #extraPackages32 = with pkgs; [
-    #  driversi686Linux.amdvlk
-    #];
-  };
-  
 #  hardware.rocm = {
 #    enable = true;
 #    package = pkgs.rocmPackages.clr;
 #  };
-  
+
   hardware.graphics.extraPackages = with pkgs; [
     vulkan-loader
     vulkan-validation-layers
     vulkan-tools
   ];
-  
+
   environment.systemPackages = with pkgs; [
     #rocm-smi
     radeontop
     clinfo
+    libva-utils
   ];
 }
