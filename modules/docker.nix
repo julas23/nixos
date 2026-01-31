f9c8fa49a95267553b6b71494ad9b787492782f2
Juliano Alves dos Santos <92172251+julas23@users.noreply.github.com>
2026-01-31 13:08:54 +0000

    Remove container configurations from docker.nix
    Removed essential container configurations for casaos, jellyfin, and nextcloud.

--- modules/docker.nix
+++ modules/docker.nix
@@ -14,48 +14,4 @@
       };
     };
   };
-  
-  # Containers essenciais
-  virtualisation.oci-containers.backend = "docker";
-  
-  virtualisation.oci-containers.containers = {
-    casaos = {
-      image = "casaos/casaos:latest";
-      ports = ["80:80"];
-      volumes = [
-        "/data/casaos:/app/data"
-        "/var/run/docker.sock:/var/run/docker.sock"
-      ];
-      extraOptions = ["--network=host"];
-    };
-    
-    jellyfin = {
-      image = "jellyfin/jellyfin:latest";
-      ports = ["8096:8096" "8920:8920"];
-      volumes = [
-        "/data/jellyfin/config:/config"
-        "/data/jellyfin/cache:/cache"
-        "/media:/media:ro"
-      ];
-      extraOptions = [
-        "--device=/dev/dri/renderD128:/dev/dri/renderD128"
-        "--device=/dev/dri/card0:/dev/dri/card0"
-      ];
-    };
-    
-    nextcloud = {
-      image = "nextcloud:latest";
-      ports = ["8080:80"];
-      volumes = [
-        "/data/nextcloud:/var/www/html"
-        "/data/nextcloud-data:/data"
-      ];
-      environment = {
-        POSTGRES_HOST = "localhost";
-        POSTGRES_DB = "nextcloud";
-        POSTGRES_USER = "nextcloud";
-        POSTGRES_PASSWORD = "nextcloud123";
-      };
-    };
-  };
 }
